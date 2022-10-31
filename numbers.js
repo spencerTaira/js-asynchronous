44f1c5ec61eb6f0fad9150e83ad58940aaf17953
michael-bocim <mtbocim@gmail.com>
2022-10-31 17:46:31 -0600

    dom updates with single fact for any amount of numbers

--- numbers.js
+++ numbers.js
@@ -1,28 +1,56 @@
 "use strict";
 
 const BASE_URL = 'http://numbersapi.com';
+const $RESULTS = $("#fav-num-facts");
 
 async function getFourFacts(evt) {
 
   evt.preventDefault();
 
-  const favNum = $('#fav-num').val();
-  const numFacts = await getFavNum(favNum);
-  console.log(numFacts);
+  const favNums = $('#fav-num').val();
+  const numFacts = await getNumsFacts(favNums);
+  //console.log(numFacts);
+  populateResults(numFacts);
 }
 
 /**
- * Makes axios post request to numbersapi with favNum and returns a
- * promise of an object containing a number fact
- * Input: favNum - String
- * Output: promise - promise (object containing num fact)
+ * Accepts an object containing number facts.
+ * Clears old results and populates dom with facts
  */
-async function getFavNum(favNum) {
+function populateResults(numFacts) {
+  $RESULTS.empty();
+  if (numFacts["text"] !== undefined) {
+    $RESULTS.append(
+      `
+      <p>${numFacts["text"]}</p>
+      `
+    )
+  } else {
+
+
+    for (let key in numFacts) {
+      $RESULTS.append(
+        `
+        <p>${numFacts[key]}</p>
+        `
+      )
+    }
+  }
+
+}
+
+
+/**
+ * Accepts one or more numbers in string form,
+ * returns an object containing the number(s) as keys and their
+ * corresponding facts
+ */
+async function getNumsFacts(favNums) {
   const response = await axios.get(
-    BASE_URL + `/${favNum}/trivia?json`
+    BASE_URL + `/${favNums}/trivia?json`
   )
 
-  return response;
+  return response.data;
 }
 
 $('#fav-num-form').on('submit', getFourFacts);
