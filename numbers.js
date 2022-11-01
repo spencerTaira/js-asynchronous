7360e3b7b21038db4c1abed9704fa7de49f55771
Spencer Taira <spencermtaira@gmail.com>
2022-10-31 17:10:09 -0700

    added multi number, multi fact functionality

--- numbers.js
+++ numbers.js
@@ -1,56 +1,67 @@
 "use strict";
 
 const BASE_URL = 'http://numbersapi.com';
-const $RESULTS = $("#fav-num-facts");
+const $RESULTS = $("#fav-num-facts"); // make it lowercase constant
 
-async function getFourFacts(evt) {
+/**
+ * Controller:
+ * On submit, calls getNumsFacts (axios request functions) and populates dom
+ * with populateResults function.
+ * Input: evt - event object
+ * Output: undefined
+ */
+async function getNumTrivia(evt) {
 
   evt.preventDefault();
 
   const favNums = $('#fav-num').val();
-  const numFacts = await getNumsFacts(favNums);
-  //console.log(numFacts);
-  populateResults(numFacts);
+
+  const fact1 = getNumsFacts(favNums); // These are promises end with 'P'
+  const fact3 = getNumsFacts(favNums);
+  const fact2 = getNumsFacts(favNums);
+  const fact4 = getNumsFacts(favNums);
+
+  const results = await Promise.allSettled([fact1, fact2, fact3, fact4]);
+  populateResults(results);
 }
 
 /**
- * Accepts an object containing number facts.
+ * Accepts an array containing objects with number facts.
  * Clears old results and populates dom with facts
  */
 function populateResults(numFacts) {
   $RESULTS.empty();
-  if (numFacts["text"] !== undefined) {
-    $RESULTS.append(
-      `
-      <p>${numFacts["text"]}</p>
-      `
-    )
-  } else {
-
-
-    for (let key in numFacts) {
-      $RESULTS.append(
+  for (const numFact of numFacts) {
+    debugger;
+    if (numFact.value["text"] !== undefined) {
+      $RESULTS.append( //Can refactor html element build to a separate function
         `
-        <p>${numFacts[key]}</p>
+        <p>${numFact.value["text"]}</p>
         `
-      )
+      );
+    } else {
+      for (let key in numFact.value) {
+        $RESULTS.append(
+          `
+          <p>${numFact.value[key]}</p>
+          `
+        );
+      }
     }
   }
-
 }
 
-
 /**
- * Accepts one or more numbers in string form,
+ * Accepts one or more numbers in string form ('1,2,3,4,5'),
  * returns an object containing the number(s) as keys and their
  * corresponding facts
  */
 async function getNumsFacts(favNums) {
   const response = await axios.get(
     BASE_URL + `/${favNums}/trivia?json`
-  )
+  );
 
   return response.data;
 }
 
-$('#fav-num-form').on('submit', getFourFacts);
+$('#fav-num-form').on('submit', getNumTrivia);
